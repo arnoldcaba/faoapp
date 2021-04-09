c92404fceee90b9a19a5fee75544b036641f38a6
Arnold Caballero <arnold.caballero@disfarma.com.co>
2021-04-09 08:13:19 -0500

    cambios en url api produccion

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9811a5b8ef478cbdd7c2a3efaba1f232.js"
+  "/precache-manifest.3d29ef05addaf6f3749d020a683f63d8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue"});
